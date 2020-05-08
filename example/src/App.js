34d54f822f378bb7a1e4309815342d7c7ffcd7d9
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2020-05-08 23:15:36 +0700

    connect all components

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import { ExampleComponent } from 'jasper'
+import { Button, AlertBox } from 'jasper'
 import 'jasper/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+const exampleButton = (
+  <Button>Hellow</Button>
+)
+
+const exampleAlertBox = (
+  <AlertBox message="Are you sure?"></AlertBox>
+)
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      toDisplay: "AlertBox"
+    }
+  }
+
+  render() {
+    const { toDisplay } = this.state
+
+    if (toDisplay === "Button") return exampleButton
+    if (toDisplay === "AlertBox") return exampleAlertBox
+  }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import React from 'react'
-import styles from './styles.module.css'
+import Button from "./Button"
+import AlertBox from "./AlertBox"
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export { Button, AlertBox }

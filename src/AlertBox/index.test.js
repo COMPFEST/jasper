9efbbf66f1e28df9c23d2983f30805bfdba79e0d
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2020-05-09 21:33:08 +0700

    add test for alertBox

--- src/AlertBox/index.test.js
+++ src/AlertBox/index.test.js
@@ -1,7 +1,14 @@
+import React from "react"
+import { shallow } from 'enzyme'
+
 import AlertBox from '.'
 
 describe('AlertBox', () => {
-  it('is truthy', () => {
-    expect(AlertBox).toBeTruthy()
+  it('renders message correctly', () => {
+    const message = "Are you really a cat?"
+    const wrapper = shallow(
+      <AlertBox message={message} />
+    )
+    expect(wrapper.contains(message)).toEqual(true)
   })
 })

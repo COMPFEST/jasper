7790e67199938687a09fb761dde89c1398ef672d
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2020-05-09 21:14:58 +0700

    add snapshot test for Button

--- src/Button/index.test.js
+++ src/Button/index.test.js
@@ -1,7 +1,15 @@
+import React from 'react'
 import Button from '.'
+import renderer from 'react-test-renderer'
 
-describe('Button', () => {
-  it('is truthy', () => {
-    expect(Button).toBeTruthy()
-  })
+it('renders correctly', () => {
+  let tree = renderer
+    .create(<Button>Hello</Button>)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+
+  tree = renderer
+    .create(<Button href="https://www.compfest.id/">Hi</Button>)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
 })

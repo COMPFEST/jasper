eb679f3d8e6c89c739afb4a158961ffbcd981aaa
Asfiolitha Wilmarani <asfiolithaw@yahoo.co.id>
2020-05-26 22:30:08 +0700

    updated smallNav test to be independent

--- src/SmallNav/index.test.js
+++ src/SmallNav/index.test.js
@@ -2,17 +2,16 @@ import React from "react"
 import renderer from 'react-test-renderer'
 
 import SmallNav from '.'
-import Card from '../Card'
 
 it('renders navItems correctly', () => {
   const navItems = [
     {
       title: "navitem1",
-      component: <Card title="Component1" content="Hello world, this is component1" />
+      component: <div>Hello world, this is component1</div>
     },
     {
       title: "navitem2",
-      component: <Card title="Component2" content="Hello world, this is component2" />
+      component: <div>Hello world, this is component2</div>
     }
   ]
 

7e47981aea94e25aed7a9c68e2ad4b7c3101b01d
Asfiolitha Wilmarani <asfiolithaw@yahoo.co.id>
2020-05-26 21:59:58 +0700

    updated checkstyle on SmallNav

--- src/SmallNav/index.test.js
+++ src/SmallNav/index.test.js
@@ -7,16 +7,16 @@ import Card from '../Card'
 it('renders navItems correctly', () => {
   const navItems = [
     {
-    title: "navitem1",
-    component: <Card title="Component1" content="Hello world, this is component1" />
+      title: "navitem1",
+      component: <Card title="Component1" content="Hello world, this is component1" />
     },
     {
       title: "navitem2",
       component: <Card title="Component2" content="Hello world, this is component2" />
-      }
+    }
   ]
 
-  let tree = renderer
+  const tree = renderer
     .create(<SmallNav navItems={navItems} />)
     .toJSON()
   expect(tree).toMatchSnapshot()

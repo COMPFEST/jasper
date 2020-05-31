7c83cc4395dfa3dd44116c11468243ab8a9f9015
MDzkM <mdzkm17@gmail.com>
2020-05-31 21:40:12 +0700

    Add Spinner component

---
+++ src/Spinner/Spinner.stories.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import Spinner from "."
+
+storiesOf("Spinner").add("Default", () => <Spinner />)
+storiesOf("Spinner").add("Variant - Regular", () => <Spinner variant="regular" />)
+storiesOf("Spinner").add("Size - Extra Small", () => <Spinner size="xs" />)
+storiesOf("Spinner").add("Size - Small", () => <Spinner size="sm" />)
+storiesOf("Spinner").add("Size - Medium", () => <Spinner size="md" />)
+storiesOf("Spinner").add("Size - Large", () => <Spinner size="lg" />)
+storiesOf("Spinner").add("Size - Extra Large", () => <Spinner size="xl" />)
+storiesOf("Spinner").add("Speed - Slow", () => <Spinner variant="regular" speed="slow" />)
+storiesOf("Spinner").add("Speed - Normal", () => <Spinner variant="regular" speed="normal" />)
+storiesOf("Spinner").add("Speed - Fast", () => <Spinner variant="regular" speed="fast" />)
+storiesOf("Spinner").add("Animation - Linear", () => <Spinner variant="regular" animation="linear" />)
+storiesOf("Spinner").add("Animation - Ease", () => <Spinner variant="regular" animation="ease" />)
+storiesOf("Spinner").add("Animation - Bouncy", () => <Spinner variant="regular" animation="bouncy" />)
---
+++ src/Spinner/index.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import PropTypes from "prop-types"
+import styles from "./styles.module.css"
+
+const Spinner = props => {
+    const { variant, size, speed, animation, ...other } = props
+    return (
+        <div className={styles.spinner}>
+            <div className={`${styles[variant]} ${styles[size]} ${styles[speed]} ${styles[animation]}`} {...other}></div>
+        </div>
+    )
+}
+
+Spinner.propTypes = {
+    variant: PropTypes.string,
+    size: PropTypes.string,
+    speed: PropTypes.string,
+    animation: PropTypes.string,
+}
+Spinner.defaultProps = {
+    variant: "default",
+    size: "md",
+    speed: "normal",
+    animation: "linear",
+}
+
+export default Spinner

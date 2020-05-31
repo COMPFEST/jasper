cc466a827bd33592196d53c73f4693837cb03cc4
MDzkM <mdzkm17@gmail.com>
2020-05-31 22:25:35 +0700

    Fix linter errors

--- src/Spinner/index.js
+++ src/Spinner/index.js
@@ -3,26 +3,29 @@ import React from "react"
 import PropTypes from "prop-types"
 import styles from "./styles.module.css"
 
-const Spinner = props => {
-    const { variant, size, speed, animation, ...other } = props
-    return (
-        <div className={styles.spinner}>
-            <div className={`${styles[variant]} ${styles[size]} ${styles[speed]} ${styles[animation]}`} {...other}></div>
-        </div>
-    )
+const Spinner = (props) => {
+  const { variant, size, speed, animation, ...other } = props
+  return (
+    <div className={styles.spinner}>
+      <div
+        className={`${styles[variant]} ${styles[size]} ${styles[speed]} ${styles[animation]}`}
+        {...other}
+      />
+    </div>
+  )
 }
 
 Spinner.propTypes = {
-    variant: PropTypes.string,
-    size: PropTypes.string,
-    speed: PropTypes.string,
-    animation: PropTypes.string,
+  variant: PropTypes.string,
+  size: PropTypes.string,
+  speed: PropTypes.string,
+  animation: PropTypes.string,
 }
 Spinner.defaultProps = {
-    variant: "default",
-    size: "md",
-    speed: "normal",
-    animation: "linear",
+  variant: "default",
+  size: "md",
+  speed: "normal",
+  animation: "linear",
 }
 
 export default Spinner
--- src/Spinner/index.test.js
+++ src/Spinner/index.test.js
@@ -1,30 +1,20 @@
-import React from 'react'
-import Spinner from '.'
-import renderer from 'react-test-renderer'
+import React from "react"
+import Spinner from "."
+import renderer from "react-test-renderer"
 
-it('renders correctly', () => {
-  let tree = renderer
-    .create(<Spinner />)
-    .toJSON()
-  expect(tree).toMatchSnapshot()
-  
-  tree = renderer
-    .create(<Spinner variant="regular" />)
-    .toJSON()
-  expect(tree).toMatchSnapshot()
-  
-  tree = renderer
-    .create(<Spinner size="lg" />)
-    .toJSON()
+it("renders correctly", () => {
+  let tree = renderer.create(<Spinner />).toJSON()
   expect(tree).toMatchSnapshot()
 
-  tree = renderer
-    .create(<Spinner speed="fast" />)
-    .toJSON()
+  tree = renderer.create(<Spinner variant="regular" />).toJSON()
   expect(tree).toMatchSnapshot()
 
-  tree = renderer
-    .create(<Spinner animation="bouncy" />)
-    .toJSON()
+  tree = renderer.create(<Spinner size="lg" />).toJSON()
+  expect(tree).toMatchSnapshot()
+
+  tree = renderer.create(<Spinner speed="fast" />).toJSON()
+  expect(tree).toMatchSnapshot()
+
+  tree = renderer.create(<Spinner animation="bouncy" />).toJSON()
   expect(tree).toMatchSnapshot()
 })

1fdcf972299b768e8963b042acb8d58b09fe020b
MDzkM <mdzkm17@gmail.com>
2020-08-04 20:14:58 +0700

    Update Spinner docs

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import AlertBox from "./AlertBox"
 import Card from "./Card"
 import Input from "./Input"
 import SmallNav from "./SmallNav"
+import Spinner from "./Spinner"
 
-export { Button, AlertBox, Card, Input, SmallNav }
+export { Button, AlertBox, Card, Input, SmallNav, Spinner }
 
 // TODO: Import Navbar and Footer
 // make sure it doesnt break the docs

c1c694cbe0e1ec61804dc70486adff93f85d0c85
andrewtheodore <andrewtheodoret@gmail.com>
2020-08-11 14:21:47 +0700

    Fix foooter page

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,12 +1,6 @@
 import React from "react"
+import SubscribeForm from "./SubscribeForm"
 import { FooterContainer } from "./style"
-import Instagram from "./Instagram.svg"
-import Facebook from "./Facebook.svg"
-import twitter from "./twitter.svg"
-import Line from "./Line.svg"
-import LinkedIn from "./LinkedIn.svg"
-import youtube from "./youtube.svg"
-import compfest from "./compfest-white.svg"
 
 const SSOButton = props => {
   return (
@@ -23,57 +17,63 @@ const SSOButton = props => {
   )
 }
 
-export const Footer = () => (
-  <FooterContainer>
-    <div className="layout" id="footer-top">
-      <div id="left">
-        <div id="logo">
-          <img src={compfest} alt="COMPFEST" />
-        </div>
-        <div id="socmed">
-          <a id="fb" href="https://www.facebook.com/COMPFEST">
-            <img src={Facebook} alt="FACEBOOK-COMPFEST" />
-          </a>
-          <a id="tw" href="https://twitter.com/COMPFEST">
-            <img src={twitter} alt="TWITTER-COMPFEST" />
-          </a>
-          <a
-            id="yb"
-            href="https://www.youtube.com/channel/UC03QmhwvQEWVI608A-RCEpw"
-          >
-            <img src={youtube} alt="YOUTUBE-COMPFEST" />
-          </a>
-          <a id="ig" href="https://www.instagram.com/compfest">
-            <img src={Instagram} alt="INSTAGRAM-COMPFEST" />
-          </a>
-          <a id="ln" href="https://line.me/R/ti/p/%40compfest">
-            <img src={Line} alt="LINE-COMPFEST" />
-          </a>
-          <a id="in" href="https://www.linkedin.com/company/compfest">
-            <img src={LinkedIn} alt="LINKEDIN-COMPFEST" />
-          </a>
-        </div>
-      </div>
-      <div id="right">
-        <div className="container-right">
-          <span id="top">COMPFEST 12 HRD Open Recruitment</span>
-          <div id="bottom">
-            <div className="link-container">
-              <p>Home</p>
-              <p>About HRD</p>
+export const Footer = (props) => {
+  return (
+    <FooterContainer>
+        <div className="row">
+          <div className="column" id="ft-left">
+            <div id="logo">
+              {/* <Link to="/"> */}
+                <img src={props.Compfest} alt="COMPFEST" />
+              {/* </Link> */}
             </div>
-            <div className="link-container">
-              <p>Departments</p>
-              <SSOButton text="Sign In" />
+            <div>
+              <h4 className="slogan">Empower Breakthrough</h4>
+              <span className="deskripsi-singkat">
+                An annual one-stop IT event run by students of Faculty of
+                Computer Science, University of Indonesia
+              </span>
             </div>
           </div>
+          <div className="column" id="ft-right">
+            <span className="ft-section-title">Find Us on Social Media!</span>
+            <div id="socmed">
+              <a id="fb" href="https://www.facebook.com/COMPFEST">
+                <img src={props.Facebook} alt="FACEBOOK-COMPFEST" />
+              </a>
+              <a id="tw" href="https://twitter.com/COMPFEST">
+                <img src={props.Twitter} alt="TWITTER-COMPFEST" />
+              </a>
+              <a
+                id="yb"
+                href="https://www.youtube.com/channel/UC03QmhwvQEWVI608A-RCEpw"
+              >
+                <img src={props.Youtube} alt="YOUTUBE-COMPFEST" />
+              </a>
+              <a id="ig" href="https://www.instagram.com/compfest">
+                <img src={props.Instagram} alt="INSTAGRAM-COMPFEST" />
+              </a>
+              <a id="ln" href="https://line.me/R/ti/p/%40compfest">
+                <img src={props.Line} alt="LINE-COMPFEST" />
+              </a>
+              <a id="in" href="https://www.linkedin.com/company/compfest">
+                <img src={props.LinkedIn} alt="LINKEDIN-COMPFEST" />
+              </a>
+            </div>
+            <span className="ft-section-title">Subscribe to COMPFEST</span>
+            <SubscribeForm />
+          </div>
         </div>
-      </div>
-    </div>
-    <div id="footer-bottom">
-      <span>© 2020 COMPFEST</span>
-    </div>
-  </FooterContainer>
-)
+        <div className="separator" />
+        <div id="footer-bottom">
+          <span id="copyright">© 2020 COMPFEST</span>
+          {/* <div id="footer-link">
+            <a href="#">Download Guidebook</a>
+            <a href="#">Contact</a>
+          </div> */}
+        </div>
+      </FooterContainer>
+  )
+}
 
 export default Footer
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import AlertBox from "./AlertBox"
 import Card from "./Card"
 import Input from "./Input"
 import SmallNav from "./SmallNav"
+import Footer from "./Footer"
 
-export { Button, AlertBox, Card, Input, SmallNav }
+export { Button, AlertBox, Card, Input, SmallNav, Footer }
 
 // TODO: Import Navbar and Footer
 // make sure it doesnt break the docs
